7a1d4eaf5b98d92a3272e23b1521d5a398ac278f
pablopaezsheridan <pablopaezsheridan@outlook.com.ar>
2024-11-11 22:17:02 -0300

    F complete

---
+++ solution/language_budget_countries.py
@@ -0,0 +1,31 @@
+from mrjob.job import MRJob
+
+class LanguageBudgetCountries(MRJob):
+    
+    def to_float(self,number):
+        try:
+            return float(number)
+        except:
+            return 0
+        
+    
+    def mapper(self, _, line):
+        fields = line.split('|')
+        language = fields[1]
+        country = fields[3] 
+        budget = self.to_float(fields[4]) 
+
+        if language != '' and country != '' and budget != 0:
+            yield language, (country, budget)
+            
+    def reducer(self, language, values):
+        countries = []
+        total_budget = 0
+        for country, budget in values:
+            if (country not in countries):
+                countries.append(country)
+            total_budget += budget
+        yield language, (countries, total_budget)
+        
+if __name__ == '__main__':
+    LanguageBudgetCountries.run()
